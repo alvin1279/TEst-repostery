71c36c0f87180ef60f67149a32d051085049f31f
av_lap <alvin.1279@gmail.com>
2020-08-08 01:45:28 +0530

    add

--- hello.py
+++ hello.py
@@ -8,11 +8,3 @@ print(sys.version)
 for i in range(3):
     print("hello world")
 
-
-print("This is a test for git")
-print("This is a test for git")
-print("This is a test for git")
-print("This is a test for git")
-print("This is a test for git")
-print("This is a test for git")
-
